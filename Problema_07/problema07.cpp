58a44fc224aa969ef239e54d3f9ced83840da32c
Ruiz Velasco <marisa.ruiz-velasco@hpe.com>
2017-05-19 15:19:49 -0500

    Solucion Problema 07

---
+++ Problema_07/problema07.cpp
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+        {
+        for(j=1;j<=n;++j)
+            {if(j<=(n-i))
+            printf(" ");
+             else
+                 printf("#");
+        }
+        printf("\n");
+    }
+   
+    return 0;
+}
